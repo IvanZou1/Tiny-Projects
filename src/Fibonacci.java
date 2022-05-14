3bcb2e0f89cf1fc97c98d302932a4fea4625db47
Ivan Zou <ivanzou2@gmail.com>
2022-05-14 19:58:04 -0400

    add fibonacci sequence

--- src/Fibonacci.java
+++ src/Fibonacci.java
@@ -1,2 +1,43 @@
+import java.util.Scanner;
+
+/*
+ * Fibonacci Sequence - Find the nth number of the fibonacci sequence and the
+ * n numbers in the sequence
+ *
+ */
+
 public class Fibonacci {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Input number for Fibonacci Sequence: ");
+        int input = sc.nextInt();
+        // O(n)
+        int[] seq = fibonacciSequence(input);
+
+        System.out.println(input + "th Number of the Fibonacci Sequence is " + seq[input - 1]);
+
+        // O(n)
+        for (int num : seq) {
+            System.out.print(num + ", ");
+        }
+    }
+
+    public static int[] fibonacciSequence(int n) {
+        if (n < 1) {
+            throw new IllegalArgumentException();
+        } else if (n == 1) {
+            return new int[]{0};
+        } else if (n == 2) {
+            return new int[]{0, 1};
+        } else {
+            int[] cache = new int[n];
+            cache[0] = 0;
+            cache[1] = 1;
+            // O(n)
+            for (int i = 2; i < cache.length; i++) {
+                cache[i] = cache[i - 1] + cache[i - 2];
+            }
+            return cache;
+        }
+    }
 }
