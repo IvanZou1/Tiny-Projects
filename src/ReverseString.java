7b7f2c5a76a59641ccaa4313048ff8cec76ada3f
Ivan Zou <ivanzou2@gmail.com>
2022-05-17 15:07:47 -0400

    added ReverseString program

---
+++ src/ReverseString.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+/*
+ *
+ * ReverseString - The user enters a string and this program outputs the
+ * string reversed
+ *
+ */
+
+public class ReverseString {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Find The Reversed String!");
+        System.out.print("Input the string to be reversed: ");
+        String in = sc.nextLine();
+        StringBuilder reversed = new StringBuilder(in).reverse();
+        System.out.println("The reversed string is \"" + reversed + "\"");
+    }
+}
