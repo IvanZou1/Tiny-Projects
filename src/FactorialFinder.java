c2e89525d2e269fbabac2d36dec170fbf9d0c862
Ivan Zou <ivanzou2@gmail.com>
2022-05-18 14:18:53 -0400

    added FactorialFinder program

---
+++ src/FactorialFinder.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+/*
+ *
+ * FactorialFinder - Find the nth factorial using both loops and recursion
+ *
+ */
+
+public class FactorialFinder {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Find the Factorial!");
+        System.out.print("Input a positive number: ");
+        int input = sc.nextInt();
+        if (input < 0) {
+            throw new IllegalArgumentException("Invalid Input: Please Enter a Positive Number");
+        }
+        System.out.println("Loop: " + input + "! = " + loopFactorial(input));
+        System.out.println("Recursion: " + input + "! = " + recursiveFactorial(input));
+    }
+
+    private static int loopFactorial(int n) {
+        if (n == 0) {
+            return 1;
+        } else {
+            int factorial = 1;
+            for (int i = 1; i <= n; i++) {
+                factorial *= i;
+            }
+            return factorial;
+        }
+    }
+
+    private static int recursiveFactorial(int n) {
+        if (n == 0) {
+            return 1;
+        } else {
+            return recursiveFactorial(n - 1) * n;
+        }
+    }
+}
