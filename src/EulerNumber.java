770d8dd67ef6146d617707c816501ca4f4df0f84
Ivan Zou <ivanzou2@gmail.com>
2022-05-18 16:38:50 -0400

    changed class name

--- src/EulerNumber.java
+++ src/EulerNumber.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
  *
  */
 
-public class EulersNumber {
+public class EulerNumber {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Find Euler's Number to the nth decimal!");
