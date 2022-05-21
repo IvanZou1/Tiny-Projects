ae61208e4929cf628fce94d1518896591426edb1
Ivan Zou <ivanzou2@gmail.com>
2022-05-21 12:02:12 -0400

    moved and renamed array of char vowels

--- src/strings/VowelCount.java
+++ src/strings/VowelCount.java
@@ -10,25 +10,26 @@ import java.util.Scanner;
  */
 
 public class VowelCount {
+    private static final char[] VOWELS = new char[]{'a', 'e', 'i', 'o', 'u'};
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Find the Number of Vowels in the String!");
         System.out.print("Input a string: ");
         String in = sc.nextLine().toLowerCase();
-        char[] vowels = new char[]{'a', 'e', 'i', 'o', 'u'};
-        int[] vowelCount = countVowels(in, vowels);
+        int[] vowelCount = countVowels(in);
         System.out.print("Vowel Count: ");
-        for (int i = 0; i < vowels.length; i++) {
-            System.out.print(vowels[i] + ": " + vowelCount[i] + " | ");
+        for (int i = 0; i < VOWELS.length; i++) {
+            System.out.print(VOWELS[i] + ": " + vowelCount[i] + " | ");
         }
         System.out.println("Total: " + vowelCount[5]);
     }
 
-    private static int[] countVowels(String input, char[] vowels) {
+    private static int[] countVowels(String input) {
         int[] vowelCount = new int[6]; // Sum of all vowels at last index
         for (int strIndex = 0; strIndex < input.length(); strIndex++) {
-            for (int vowelIndex = 0; vowelIndex < vowels.length; vowelIndex++) {
-                if (input.charAt(strIndex) == vowels[vowelIndex]) {
+            for (int vowelIndex = 0; vowelIndex < VOWELS.length; vowelIndex++) {
+                if (input.charAt(strIndex) == VOWELS[vowelIndex]) {
                     vowelCount[vowelIndex]++;
                     vowelCount[5]++;
                 }
