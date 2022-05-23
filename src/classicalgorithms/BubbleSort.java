bdbcc6c956caa4766beb29b6742b72bba927b722
Ivan Zou <ivanzou2@gmail.com>
2022-05-23 08:20:45 -0400

    added BubbleSort program

---
+++ src/classicalgorithms/BubbleSort.java
@@ -0,0 +1,71 @@
+package classicalgorithms;
+
+import java.util.Scanner;
+
+/*
+ *
+ * BubbleSort - An iterative algorithm that sorts an integer array
+ * in increasing order in O(n^2) time
+ *
+ */
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Sort an integer array using Bubble Sort!");
+        System.out.println("Input the number of integers in the array: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Input the " + n + " integers for the array: ");
+        for (int i = 0; i < n; i++) {
+            System.out.print(i + 1 + ") ");
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("Your Original Array: " + arrToString(arr));
+        bubbleSort(arr);
+        System.out.println("The Sorted Array: " + arrToString(arr));
+        sc.close();
+    }
+
+    private static String arrToString(int[] arr) {
+        StringBuilder str = new StringBuilder("[");
+        for (int i = 0; i < arr.length; i++) {
+            if (i == arr.length - 1) {
+                str.append(arr[i]);
+            } else {
+                str.append(arr[i]);
+                str.append(", ");
+            }
+        }
+        str.append("]");
+        return str.toString();
+    }
+
+    // O(n^2)
+    private static void bubbleSort(int[] arr) {
+        while (!isSorted(arr)) {
+            for (int i = 0; i < arr.length - 1; i++) {
+                if (arr[i] > arr[i + 1]) {
+                    swap(arr, i, i + 1);
+                }
+            }
+        }
+    }
+
+    // O(n)
+    private static boolean isSorted(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            if (arr[i] > arr[i + 1]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // O(1)
+    private static void swap(int[] arr, int index1, int index2) {
+        int temp = arr[index1];
+        arr[index1] = arr[index2];
+        arr[index2] = temp;
+    }
+}
